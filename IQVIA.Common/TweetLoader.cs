cb446cfb2ea155267e76cb36b5ba9c3b6f807ace
Anton <unsend2@gmail.com>
2018-08-20 13:53:32 +0300

    changed loading algorithm

--- IQVIA.Common/TweetLoader.cs
+++ IQVIA.Common/TweetLoader.cs
@@ -23,7 +23,7 @@ namespace IQVIA.Common
         /// <param name="endDate">Latest entry timestamp in UTC, inclusive</param>
         /// <param name="checkForDuplicates">Remove duplicates from response</param>
         /// <returns></returns>
-        public async Task<TweetLoaderResponse> LoadTweetsByDate(DateTime startDate, DateTime endDate, bool removeDuplicates = true)
+        public async Task<TweetLoaderResponse> LoadTweetsByDate(DateTime startDate, DateTime endDate)
         {
             if(startDate > endDate)
             {
@@ -34,7 +34,7 @@ namespace IQVIA.Common
             }
             try
             {
-                return await LoadTweetsByDateInternal(startDate, endDate, removeDuplicates);
+                return await LoadTweetsByDateInternal(startDate, endDate);
             }
             catch(Exception ex)
             {
@@ -45,12 +45,13 @@ namespace IQVIA.Common
             }
         }
 
-        private async Task<TweetLoaderResponse> LoadTweetsByDateInternal(DateTime startDate, DateTime endDate, bool removeDuplicates = true)
+        private async Task<TweetLoaderResponse> LoadTweetsByDateInternal(DateTime startDate, DateTime endDate)
         {
             DateTime _startDate = startDate;
             IEnumerable<Tweet> resultTweets = new List<Tweet>();
-            List<string> loadedIds = new List<string>();
+            Dictionary<string, int> loadedIds = new Dictionary<string, int>();
             bool endLoading = false;
+            bool removeDuplicates = true;
             while (!endLoading)
             {
                 List<Tweet> tweetsData;
@@ -67,18 +68,20 @@ namespace IQVIA.Common
                 }
                 if (tweetsData != null && tweetsData.Count > 0)
                 {
+                    // left removeDuplicates variable for test purposes
+                    // we use dictionary for duplicate search
                     if (removeDuplicates)
                     {
                         List<Tweet> filteredTweets = new List<Tweet>();
                         foreach (var tweet in tweetsData)
                         {
-                            if (loadedIds.Contains(tweet.Id))
+                            if (loadedIds.ContainsKey(tweet.Id))
                             {
-                                // count amount of duplicates ?
+                                loadedIds[tweet.Id]++; // count amount of duplicates
                             }
                             else
                             {
-                                loadedIds.Add(tweet.Id);
+                                loadedIds[tweet.Id] = 1;
                                 filteredTweets.Add(tweet);
                             }
                         }
@@ -88,8 +91,16 @@ namespace IQVIA.Common
                     {
                         resultTweets = resultTweets.Concat(tweetsData);
                     }
-                    _startDate = tweetsData.Last().Stamp.AddTicks(1);
-                    endLoading = tweetsData.Count < _tweetService.ResponseTweetCount || _startDate >= endDate;
+                    // we can use two options
+                    // 1. non-overlapping intervals (using .AddTicks(1))
+                    // 2. overlapping intervals with explicit duplicates removal
+                    // option 1 will be incorrect in cases with two unique tweets having fully equal stamps (including ticks)
+                    // option 2 will be incorrect in cases, when we have more than 100 tweets with same stamp
+                    // both of them are equally impossible, so for a basic working version, we use second option
+                    _startDate = tweetsData.Last().Stamp;
+                    // we assume that _startDate will always be greater than startDate and change only in the direction of endDate
+                    // (meaning, our service can't return a tweet from 2014 when we search for tweets from 2016 to 2018)
+                    endLoading = tweetsData.Count < _tweetService.ResponseTweetCount || _startDate > endDate;
                 }
                 else
                 {
